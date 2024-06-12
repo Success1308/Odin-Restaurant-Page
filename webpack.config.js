98d59bc202f8b2609aec4cb23c9c7ed7d16d651c
Success Ambaliya <success.ambaliya13@gmail.com>
2024-06-11 22:15:26 -0400

    Restaurant-Page

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+
+  plugins: [
+	new HtmlWebpackPlugin({
+	  title: 'Restaurant Web Page',
+	  template: './src/index.html',
+	}),
+  ],
+  mode: 'development', 
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+    
+    ],
+  },
+};
